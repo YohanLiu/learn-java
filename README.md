d6cec7f1aa82a0d8d1324b3e45a54e3b249451e7
yohan <yohan@yohandeMacBook-Pro.local>
2023-12-12 22:07:07 +0800

    修改项目报红添加 readme

---
+++ README.md
@@ -0,0 +1,2 @@
+spring boot 初始化项目
+java 基础练习
--- src/test/java/com/example/javabasic/JavaBasicApplicationTests.java
+++ src/test/java/com/example/javabasic/JavaBasicApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JavaBasicApplicationTests {
+public class JavaBasicApplicationTests {
 
     @Test
     void contextLoads() {
