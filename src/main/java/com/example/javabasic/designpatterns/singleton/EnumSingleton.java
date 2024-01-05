b477a2f32621e3af611c032c6ba8074ae3a881d6
刘寅厚 <yinhou.liu@huolala.cn>
2024-01-05 10:34:27 +0800

    单例模式

---
+++ src/main/java/com/example/javabasic/designpatterns/singleton/EnumSingleton.java
@@ -0,0 +1,52 @@
+package com.example.javabasic.designpatterns.singleton;
+
+/**
+ * 枚举实现单例模式.
+ *
+ * @author yinhou.liu
+ * @Date 2024/01/05
+ */
+public class EnumSingleton {
+    /**
+     * 私有化构造函数.
+     */
+    private EnumSingleton() {
+    }
+
+    /**
+     * 对外方法,获取 EnumSingleton 类.
+     *
+     * @return {@link EnumSingleton}
+     */
+    public static EnumSingleton getInstance() {
+        return Singleton.INSTANCE.getInstance();
+    }
+
+
+    /**
+     * 静态枚举类实现单例.
+     */
+    static enum Singleton {
+        /**
+         * 枚举对象,天生为单例.
+         */
+        INSTANCE;
+
+        private EnumSingleton enumSingleton;
+
+        /**
+         * 私有化枚举的构造函数.
+         */
+        private Singleton() {
+            enumSingleton = new EnumSingleton();
+        }
+
+        public EnumSingleton getInstance() {
+            return enumSingleton;
+        }
+    }
+
+}
+
+
+
