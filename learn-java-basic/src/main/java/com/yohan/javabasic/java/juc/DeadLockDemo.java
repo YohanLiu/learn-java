ed3787c5e09175ed0a146e4a5c89a6280112959b
Yohan <yohan_liu@foxmail.com>
2024-03-10 23:13:06 +0800

    死锁示例

---
+++ learn-java-basic/src/main/java/com/yohan/javabasic/java/juc/DeadLockDemo.java
@@ -0,0 +1,41 @@
+package com.yohan.javabasic.java.juc;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 死锁demo.
+ */
+public class DeadLockDemo {
+    public static void main(String[] args) {
+        final Object objectA = new Object();
+        final Object objectB = new Object();
+
+        new Thread(() -> {
+            synchronized (objectA) {
+                System.out.println(Thread.currentThread().getName() + "线程\t 自己持有A锁，希望获得B锁");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (objectB) {
+                    System.out.println(Thread.currentThread().getName() + "线程\t 成功获得B锁");
+                }
+            }
+        }, "A").start();
+
+        new Thread(() -> {
+            synchronized (objectB) {
+                System.out.println(Thread.currentThread().getName() + "线程\t 自己持有B锁，希望获得A锁");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (objectA) {
+                    System.out.println(Thread.currentThread().getName() + "线程\t 成功获得A锁");
+                }
+            }
+        }, "B").start();
+    }
+}
