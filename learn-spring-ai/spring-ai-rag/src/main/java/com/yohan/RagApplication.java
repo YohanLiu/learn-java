4ea5b6c4269d9f0ecd675de9623670f7073d1070
Yohan <2220041897@qq.com>
2025-06-19 23:26:02 +0800

    spring ai rag

---
+++ learn-spring-ai/spring-ai-rag/src/main/java/com/yohan/RagApplication.java
@@ -0,0 +1,16 @@
+package com.yohan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yohan
+ * @Date 2025/06/19
+ */
+@SpringBootApplication
+public class RagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagApplication.class, args);
+    }
+}
